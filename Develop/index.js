012098fa87302430449270b4ce553acf47b4ce96
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-01-16 21:42:14 -0600

    Just doing a quick save before I try some drastic changes.

--- Develop/index.js
+++ Develop/index.js
@@ -21,7 +21,7 @@ const pdf = require('puppeteer');
 const writeFileAsync = util.promisify(fs.writeFile);
 
 //==============================================
-// Inquirer asks for GHUsername, Color, Location, bio, linkedIn, 
+// Inquirer asks for GHUsername, Color, Location, bio, and linkedIn
 //==============================================
 function promptUser() {
     return inquirer.prompt([
@@ -59,3 +59,28 @@ function promptUser() {
 
     ]);
 }
+function generateHTML(answers) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.username}</h1>
+      <p class="lead">I am from ${answers.loc}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+  }
