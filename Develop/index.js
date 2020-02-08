8505fc25952d325c1191f65e09a6d9339c6d5cc0
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-02-08 16:39:10 -0600

    Got the color to work on the html. My html to pdf is not pulling colors though.

--- Develop/index.js
+++ Develop/index.js
@@ -2,18 +2,17 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const axios =require("axios");
+const axios = require("axios");
 var convertFactory = require("electron-html-to")
 const open = require("open");
 var path = require("path")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 var generateHTML = require("./generateHTML");
-var api = require("./api")
 //==============================================
 // Inquirer asks for GHUsername, Color, Location, bio, and linkedIn
 //==============================================
-var questions =[
+var questions = [
   {
     type: "input",
     message: "What is your Github Username?",
@@ -32,62 +31,65 @@ var questions =[
     ]
   }
 
+
 ];
 
-function start(){
-  inquirer.prompt(questions).then(({username,color}) => {
+function start() {
+  inquirer.prompt(questions).then(({ username, color }) => {
     console.log(username)
     console.log(color);
-    getUserInfo(username,color); 
+    getUserInfo(username, color);
   })
 }
-function getUserInfo(username,color){
-  
-   axios.get("https://api.github.com/users/joshkuruvilla512").then(function(res){
+function getUserInfo(username, color) {
+
+  axios.get("https://api.github.com/users/joshkuruvilla512").then(function (res) {
     //console.log(res)
-    followerCount = res.data.followers
-      var data = {
-        followers: res.data.followers,
-        following: res.data.following,
-        publicRepos : res.data.public_repos
-      }
-      getStarsInfo(username,data,color)
-      
-    
+    // followerCount = res.data.followers
+    var data = {
+      followers: res.data.followers,
+      following: res.data.following,
+      location: res.data.location,
+      github: res.data.html_url,
+      linkedin: res.data.blog,
+      publicRepos: res.data.public_repos
+    }
+    // console.log(data);
+    getStarsInfo(username, data, color)
+
+
     //console.log("data in api: " +JSON.stringify(data))
   })
-  
+  // joshkuruvilla512
 }
-function getStarsInfo(username, data,color){
+function getStarsInfo(username, data, color) {
   var starCount
-  axios.get("https://api.github.com/users/joshkuruvilla512/starred").then(function(res){
-            console.log(res.data[0].stargazers_count)
-             var starCount = res.data[0].stargazers_count
-             console.log("star count: "+starCount)
-             var info = {
-               star: starCount,
-               following: data.following,
-               followers:data.followers,
-               total_repos: data.publicRepos
-             }
-             console.log(info)
-             var html = generateHTML({username,color, ...info})
-             const conversion = convertFactory({
-               converterPath: convertFactory.converters.PDF
-             })
-             conversion({ html}, function(err, result) {
-              if (err) {
-                return console.error(err);
-              }
-          
-              result.stream.pipe(fs.createWriteStream(path.join(__dirname, "resume.pdf")));
-              conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
-            });
-            open(path.join(process.cwd(), "resume.pdf"))
-  
-        })
+  axios.get("https://api.github.com/users/joshkuruvilla512/starred").then(function (res) {
+    console.log(res.data[0].stargazers_count)
+    var starCount = res.data[0].stargazers_count
+    console.log("star count: " + starCount)
+    data.star = starCount
+    //  console.log(info)
+    var html = generateHTML({ username, color, ...data })
+    fs.writeFile('newindex.html', html, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+    const conversion = convertFactory({
+      converterPath: convertFactory.converters.PDF
+    })
+    conversion({ html }, function (err, result) {
+      if (err) {
+        return console.error(err);
+      }
+
+      result.stream.pipe(fs.createWriteStream(path.join(__dirname, "resume.pdf")));
+      conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+      open(path.join(process.cwd(), "resume.pdf"))
+    });
+
+  })
 }
 
 
 start()
- 
